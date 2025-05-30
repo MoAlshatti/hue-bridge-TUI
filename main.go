1abdd363d7aef54b7dea684c6390b8b169e80e15
Mohammad Alshatti <lg22416@bristol.ac.uk>
2025-05-30 12:30:20 +0100

    setup the boilerplate and infrastructure

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m model) View() string {
+	return " "
+}
+
+func main() {
+
+}
